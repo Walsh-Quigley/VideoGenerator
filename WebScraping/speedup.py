7358c280a10a6f08863287f8f35f362ec6cb2042
Walsh Quigley <86450744+Walsh-Quigley@users.noreply.github.com>
2024-05-08 15:44:37 -0700

    Cleaned

--- WebScraping/speedup.py
+++ WebScraping/speedup.py
@@ -6,13 +6,13 @@ import os
 
 def speedUp():
 
-    file_path1 = r"c:/Users/Walsh Quigley/Desktop/PersonalProjects/WebScraping/narration/rawTitle.mp3"
+    file_path1 = r"c:/Users/Walsh Quigley/Desktop/PersonalProjects/VideoGenerator/WebScraping/narration/rawTitle.mp3"
     # Load the audio file
     audio = AudioSegment.from_file(file_path1, "mp3")
     new_title = speedup(audio, 1.1 , 110)
     
 
-    file_path2 = r"c:/Users/Walsh Quigley/Desktop/PersonalProjects/WebScraping/narration/rawBody.mp3"
+    file_path2 = r"c:/Users/Walsh Quigley/Desktop/PersonalProjects/VideoGenerator/WebScraping/narration/rawBody.mp3"
     audio = AudioSegment.from_file(file_path2, "mp3")
     new_body = speedup(audio, 1.1 , 110)
     
@@ -21,10 +21,10 @@ def speedUp():
     combined.export('./narration/final.mp3', format="mp3")
 
 def combine():
-    file_path1 = r"c:/Users/Walsh Quigley/Desktop/PersonalProjects/WebScraping/narration/rawTitle.mp3"
+    file_path1 = r"c:/Users/Walsh Quigley/Desktop/PersonalProjects/VideoGenerator/WebScraping/narration/rawTitle.mp3"
     title = AudioSegment.from_file(file_path1, "mp3")
 
-    file_path2 = r"c:/Users/Walsh Quigley/Desktop/PersonalProjects/WebScraping/narration/rawBody.mp3"
+    file_path2 = r"c:/Users/Walsh Quigley/Desktop/PersonalProjects/VideoGenerator/WebScraping/narration/rawBody.mp3"
     body = AudioSegment.from_file(file_path2, "mp3")
 
     combined = title + body
